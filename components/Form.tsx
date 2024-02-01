e7acb6f99cc23b393cbc5411cc0849eb240a84a4
vlad-guzun <mernapplications@gmail.com>
2024-02-01 14:55:26 -0800

    c

---
+++ components/Form.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form>
+        <input placeholder='category' />
+        <textarea cols={4} rows={10}/> 
+    </form>
+  )
+}
+
+export default Form
